853bf7790ef79ae61bc28c8ec3c20b64164e8222
Brennan Colberg <brennan.colberg@gmail.com>
2020-12-28 18:39:34 -0800

    add firebase project

--- firebase/index.ts
+++ firebase/index.ts
@@ -1,7 +1,10 @@
 const firebaseConfig = {
-  // TODO create a new Firebase project
-  // TODO make a "Web App" within your Firebase project
-  // TODO put the firebaseConfig for your Web App here
+  apiKey: "AIzaSyAyklz_9eWP_mOdzPPXEnIC1pIj5isbEyY",
+  authDomain: "firestore-write-stress-test.firebaseapp.com",
+  projectId: "firestore-write-stress-test",
+  storageBucket: "firestore-write-stress-test.appspot.com",
+  messagingSenderId: "621701226836",
+  appId: "1:621701226836:web:1747cdb150cae6aa65e8c1",
 }
 
 import firebase from "firebase/app"
